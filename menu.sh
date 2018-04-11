5999eed29d18a8d4a4588f8f5668c43c11ffe151
Henrique-Martins <36017133+Henrique-Martins@users.noreply.github.com>
2018-04-11 16:50:17 -0300

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,8 @@
+echo '--------------------------------------------------'
+echo 'I (a) =netstat -a |todas as conexões             I'
+echo 'I (b) =netstat -at|todas as conexoes tcp         I'
+echo 'I (c) =netstat -au|todas as conexoes udp         I'
+echo 'I (d) =netstat -aw|todas as conexoes raw         I'
+echo 'I (e) =netstat -aut|todas as conexoes tcp e udp  I'
+echo 'I Pressione CRTL+C para sair                     I'
+echo '--------------------------------------------------'
